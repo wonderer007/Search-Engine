1be92247f369b669dbb95f3cd3c460696c7307b2
haider <haider@haider.(none)>
2014-05-03 01:42:36 +0500

    few changes

--- index.py
+++ index.py
@@ -3,37 +3,45 @@ from constants import *
 from TwitterAPI import TwitterAPI
 from scrap import Scraper
 import time
+import logging
 import json
 import urllib2
 
-#api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, Access_token, Access_token_secret)
-count , max_id = init()
-count
 
-for x in range(30):
+
+# get logging handler
+logger = logging.getLogger(logger)
+hdlr = logging.FileHandler(log_file)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+
+count , max_id = init()
+
+for x in range(2):
     if x%5 is 0:
         api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, Access_token, Access_token_secret)
 
-    if x%15 is 0:
-        save_config(count, max_id)
-
     json_str = {}
     json_str['q'] = search_query
-    json_str['count'] = "50"
+    json_str['count'] = "100"
     if max_id is not None:
     	json_str['max_id'] = max_id
-
+    
     try:
         r = api.request('search/tweets', json_str)
     except:
         pass
+
     for item in r.get_iterator():
         max_id = item['id']
         if item['entities']['urls']:
             max_id = item['id']
-            item['doc_id'] = count
+            item['ser_id'] = count
             count +=1
-            thread = Scraper(count, item['entities']['urls'][0]['expanded_url'], item)
+            thread = Scraper(item['entities']['urls'][0]['expanded_url'], item)
             thread.start()
+            if count%5 is 0:
+                save_config(count, max_id)
 
     time.sleep(3)
--- scrap.py
+++ scrap.py
@@ -1,23 +1,36 @@
 from bs4 import BeautifulSoup
 from mongo import MongoDB
 from constants import *
+import logging
 import urllib2
 import urllib
 import threading
 import json
 
 
+# get logging handler
+logger = logging.getLogger(logger)
+hdlr = logging.FileHandler(log_file)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
 
 class Scraper(threading.Thread):
-	
+
+	logger.info("Connecting with mongo database with parameters .....")
+	logger.info("host %s" %(mongo_host))
+	logger.info("port %s" %(mongo_port))
+	logger.info("database %s" %(mongo_database))
+
 	mongo = MongoDB(mongo_host, mongo_port, mongo_database, mongo_collection)
 	tweet = mongo.db.tweets
 	urls = {}
+	count = 1
 
-	def __init__(self, _id, _url, _json):
+
+	def __init__(self, _url, _json):
 		threading.Thread.__init__(self)
 		self.url = _url
-		self.id = _id
 		self.json = _json
 		try:
 			resp = urllib.urlopen(self.url)
@@ -30,15 +43,20 @@ class Scraper(threading.Thread):
 	
 		if self.urls.has_key(self.url):
 			print "URL %s has already been visited " % (self.url)
+			logger.info("URL %s has already been visited " % (self.url))
 			return
 
 		self.urls[self.url] = True
 		header = {'User-Agent': 'Mozilla/5.0'}
 		req = urllib2.Request(self.url,headers=header)
 		try:
+			logger.info("Making HTTP request for url %s " % (self.url))
 			page = urllib2.urlopen(req)
 		except:
+			logger.info("HTTP request for url %s is crashed" % (self.url))
 			return
+
+		logger.info("HTTP Request for url %s successfull" % (self.url))
 		soup = BeautifulSoup(page)
 
 		# kill all script and style elements
@@ -49,12 +67,15 @@ class Scraper(threading.Thread):
 		text = soup.get_text()
 
 		# break into lines and remove leading and trailing space on each
-		lines = (line.strip() for line in text.splitlines())
+		lines 	= (line.strip() for line in text.splitlines())
 		# break multi-headlines into a line each
-		chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+		chunks 	= (phrase.strip() for line in lines for phrase in line.split("  "))
 		# drop blank lines
-		text = '\n'.join(chunk for chunk in chunks if chunk)
-		fin = open(base_dir+ str(self.id), "w")
+		text 	= '\n'.join(chunk for chunk in chunks if chunk)
+		fin 	= open(base_dir+ str(Scraper.count), "w")
+		print Scraper.count
+		self.json['doc_id'] = Scraper.count
+		Scraper.count +=1
 		self.tweet.insert(self.json)
 		print "Tweet inserted in mongo"
 		fin.write(text.encode('utf-8'))
