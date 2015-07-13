785737eb0b7f69af3594c9bd23e0588a5e44fc35
Haider Ali <alihaider907@gmail.com>
2015-07-13 05:49:15 +0500

    twitter app credentials removed

--- constants.py
+++ constants.py
@@ -6,10 +6,10 @@ def get_cities():
     return cities
 
 
-Access_token        = "171007935-2P970TAIQYIdS4lxqp8x6ZzX58eHMc2blmBwLoA7"
-Access_token_secret = "uSDbw9JMJIECHlD5WLeymCwaiP99pnzu8XkfxJJ4GHwRk"
-CONSUMER_KEY        = "FphPZ38AD3wHRfL7JjxHyQ"
-CONSUMER_SECRET     = "FfGDAXDiEdupjn2Bmy9rS1byCAj2gAVzeLB5b6LOo"
+Access_token        = "your-access-token"
+Access_token_secret = "your access secret"
+CONSUMER_KEY        = "your consumer key"
+CONSUMER_SECRET     = "your consumer secret"
 
 mongo_database = "twitter"
 mongo_collection = "tweets"
